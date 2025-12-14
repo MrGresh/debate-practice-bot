9e5e129b127da636ffc7a6f5ed5fd6c51112cd28
Gresh <gresh@myanatomy.in>
2025-12-14 11:25:30 +0530

    Update vapi.constants.js

--- backend/src/constants/vapi.constants.js
+++ backend/src/constants/vapi.constants.js
@@ -2,12 +2,12 @@
 
 const VAPI_CONFIG = {
   VAPI_AUTH_HEADER: {
-    Authorization: `Bearer ${process.env.VAPI_PRIVATE_KEY || '0e5a5f7d-ceb8-4840-bca8-4906989eef03'}`,
+    Authorization: `Bearer ${process.env.VAPI_PRIVATE_KEY}`,
     "Content-Type": "application/json",
   },
-  VAPI_PUBLIC_KEY: process.env.VAPI_PUBLIC_KEY || 'd11a3eb0-b3e5-4231-ac99-32b2bcdb2ce6',
+  VAPI_PUBLIC_KEY: process.env.VAPI_PUBLIC_KEY,
   VAPI_BASE_URL: "https://api.vapi.ai/",
-  VAPI_PASSKEY: process.env.VAPI_PASSKEY || 1234
+  VAPI_PASSKEY: process.env.VAPI_PASSKEY
 };
 
 module.exports = {
